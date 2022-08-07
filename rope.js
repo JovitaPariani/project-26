cef4ff0a45b76662a8ee571693e19dbd8574dfc4
JovitaPariani <104781459+JovitaPariani@users.noreply.github.com>
2022-08-07 11:14:04 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,26 @@
+class rope{
+	constructor(body1,body2, offsetX)
+	{
+		var options={
+			bodyA:body1,
+			bodyB:body2
+		}
+
+		this.rope=Constraint.create(options)
+		World.add(world,this.rope)
+
+		this.offsetX = offsetX
+	}
+
+	display()
+	{
+		var pointA=this.rope.bodyA.position;
+		var pointB=this.rope.bodyB.position;
+
+		strokeWeight(2);
+
+		//WRITE THE CORRECT CODE TO RENDER A LINE BETWEEN THE TWO BODIES
+line(con.point.A.x,con.pointA.y,this.rope.bodyA.position,this.rope.bodyB.position)
+	}
+
+}
